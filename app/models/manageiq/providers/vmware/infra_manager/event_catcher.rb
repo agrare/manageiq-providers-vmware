3ad9d883aa6b22332b948d9311766507025c8c0d
Adam Grare <agrare@redhat.com>
2019-12-02 11:44:10 -0500

    Start RefreshWorker for events, disable event catcher

--- app/models/manageiq/providers/vmware/infra_manager/event_catcher.rb
+++ app/models/manageiq/providers/vmware/infra_manager/event_catcher.rb
@@ -1,3 +1,5 @@
 class ManageIQ::Providers::Vmware::InfraManager::EventCatcher < ManageIQ::Providers::BaseManager::EventCatcher
   require_nested :Runner
+
+  self.required_roles = []
 end
--- app/models/manageiq/providers/vmware/infra_manager/refresh_worker.rb
+++ app/models/manageiq/providers/vmware/infra_manager/refresh_worker.rb
@@ -1,3 +1,5 @@
 class ManageIQ::Providers::Vmware::InfraManager::RefreshWorker < ManageIQ::Providers::BaseManager::RefreshWorker
   require_nested :Runner
+
+  self.required_roles = %w[ems_inventory event]
 end
